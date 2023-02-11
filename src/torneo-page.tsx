e195af1f7a7042678156e3a5870432ebd27b4448
mmanna3 <mmmannna3@gmail.com>
2023-02-11 17:26:40 -0300

    Fetchea torneos

--- src/torneo-page.tsx
+++ src/torneo-page.tsx
@@ -1,4 +1,22 @@
+import { useEffect } from 'react';
+
 function Torneo() {
+  async function fetchTorneosAperturaClausura() {
+    const response = await fetch(
+      'https://www.edefi.com.ar/publico/TorneosAperturaClausura?anio=2022',
+      {
+        method: 'get',
+      },
+    );
+
+    const torneos = await response.json();
+    console.log(torneos);
+  }
+
+  useEffect(() => {
+    fetchTorneosAperturaClausura();
+  }, []);
+
   return <div>Torneitos</div>;
 }
 
