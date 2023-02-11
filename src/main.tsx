28c06e989e11e3c3a1022f3754c7cd6d9289bd76
mmanna3 <mmmannna3@gmail.com>
2023-02-11 15:15:41 -0300

    Instala react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import TorneosPage from './torneo-page';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './error-page';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/torneos',
+    element: <TorneosPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
---
+++ src/torneo-page.tsx
@@ -0,0 +1,5 @@
+function Torneo() {
+  return <div>Torneitos</div>;
+}
+
+export default Torneo;
