1febc984f76ad8cf737e1eb927e33feedef1b024
Elroli09 <yoldi.gustavo@gmail.com>
2023-02-27 15:03:17 -0300

    TablaDePosiciones recibe el array por props y renderiza un elemento 'tabla' con los datos de la categoría

--- src/components/ListaDeTablasDePosiciones.tsx
+++ src/components/ListaDeTablasDePosiciones.tsx
@@ -1,40 +1,33 @@
-import { render } from 'react-dom';
-import { Renglon } from '../interfaces/Renglon';
 import { TablasPorCategoria } from '../interfaces/TablasPorCategoria';
 
 interface Props {
   tablasPorCategoria: TablasPorCategoria[];
   tabla: TablasPorCategoria;
-  // tabla: Renglon;
+  key: number;
+  categoria: number;
 }
 
 export default function ListaDeTablasDePosiciones({ tablasPorCategoria }: Props) {
-  {
-    tablasPorCategoria.forEach((tabla) => <TablaDePosiciones key={tabla.CategoriaId}  />);
-  }
-
   return (
-    <div className='flex bg-gray-400'>
-      <TablaDePosiciones />
-      <TablaDePosiciones />
+    <div className='flex flex-wrap justify-center bg-gray-400'>
+      {tablasPorCategoria.map((tabla) => (
+        <TablaDePosiciones key={tabla.CategoriaId} categoria={tabla.Categoria} tabla={tabla} />
+      ))}
     </div>
   );
 }
 
-function TablaDePosiciones() {
-
-
-
+function TablaDePosiciones({ tabla, categoria }: Props) {
   return (
     <div className='m-2'>
-      <h2>Numero de Tabla: 15</h2>
+      <h2>Numero de Tabla: {categoria}</h2>
 
-      <table className='table-auto bg-white'>
-        <thead>
-          <tr>
+      <table className='table-auto grid bg-white'>
+        <thead >
+          <tr className='grid grid-cols-12'>
             <th>Pos</th>
             <th>Esc</th>
-            <th>Equipo</th>
+            <th className='col-span-4'>Equipo</th>
             <th>J</th>
             <th>G</th>
             <th>E</th>
@@ -43,40 +36,21 @@ function TablaDePosiciones() {
             <th>Pts</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Escudo</td>
-            <td>San Lorenzo</td>
-            <td>11</td>
-            <td>11</td>
-            <td>61</td>
-            <td>91</td>
-            <td>91</td>
-            <td>11</td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>Escudo</td>
-            <td>San Lorenzo</td>
-            <td>11</td>
-            <td>11</td>
-            <td>61</td>
-            <td>91</td>
-            <td>91</td>
-            <td>11</td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>Escudo</td>
-            <td>San Lorenzo</td>
-            <td>11</td>
-            <td>11</td>
-            <td>61</td>
-            <td>91</td>
-            <td>91</td>
-            <td>11</td>
-          </tr>
+
+          <tbody >
+        {tabla.Renglones.map((renglon) => (
+            <tr key={renglon.EquipoId} className='grid grid-cols-12'>
+              <td>{renglon.Posicion}</td>
+              <td >{renglon.Escudo}</td>
+              <td className='col-span-4'>{renglon.Equipo}</td>
+              <td>{renglon.Pj}</td>
+              <td>{renglon.Pg}</td>
+              <td>{renglon.Pe}</td>
+              <td>{renglon.Pp}</td>
+              <td>{renglon.Np}</td>
+              <td>{renglon.Pts}</td>
+            </tr>
+        ))}
         </tbody>
       </table>
     </div>
