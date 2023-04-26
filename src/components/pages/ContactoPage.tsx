5510c6e8fab145fc8e797c01782ae70dca4b0837
Elroli09 <yoldi.gustavo@gmail.com>
2023-04-26 20:34:23 -0300

    Carga imágenes en contacto page.

--- src/components/pages/ContactoPage.tsx
+++ src/components/pages/ContactoPage.tsx
@@ -1,26 +1,37 @@
+import IMG_CONTACTO_LARGE from '../../assets/images/mobile/buttons/contacto-large.png'
+import SEGUINOS_CELU from '../../assets/images/mobile/seguinos-celu.png'
+import DATOS_CONTACTO_CELU from '../../assets/images/mobile/datos-contacto-celu.png'
+
 export const ContactoPage = () => {
   return (
     <main>
-      <h1 className='mt-60 text-center text-3xl font-bold underline'>ContactoPage</h1>
+      <img src={IMG_CONTACTO_LARGE} alt="Contacto" />
+      <img src={DATOS_CONTACTO_CELU} alt="Datos" />
+      {/* <div>
+        <h2>Horarios de atención</h2>
+        <p>Lunes a jueves de 17:30 hs a 20:30 hs</p>
+        <p>Viernes de 17:00 hs a 19:30 hs</p>
+      </div>
+      <div>
+        <h2>Dirección</h2>
+        <p>Juan B. Justo 550, Haedo. Bs. As</p>
+        <p>CP: 1706</p>
+      </div>
+      <div>
+        <h2>Teléfono</h2>
+        <p>21958389</p>
+      </div>
+      <div>
+        <h2>E-mail</h2>
+        <p>edefiargentina@hotmail.com</p>
+      </div> */}
+      <img src={SEGUINOS_CELU} alt="Seguinos" />
     </main>
   );
 };
 
-{
-  /* <section className='flex justify-between bg-gray-50 p-10'>
-          <img className='' src='./vite.svg' alt='logo' />
-          <div>
-            <h3 className='font-semibold'>Contacto</h3>
-            <p>@liga_edefi</p>
-            <p>Juan B. justo 550, Haedo, Bs. As.</p>
-            <p>Juan B. justo 550, Haedo, Bs. As.Lunes a Jueves de 17.30 a 20.30 hs.</p>
-            <p>Viernes 17.30 a 19.30 hs.</p>
-            <p>(+549) 2195-8389</p>
-            <p>edefiargentina@hotmail.com</p>
-          </div>
-          <div>
-            <h3 className='font-semibold'>Seguinos</h3>
-            <img className='h-[400px] w-[200px]' src='./vite.svg' alt='phone' />
-          </div>
-        </section> */
-}
+
+
+
+
+
