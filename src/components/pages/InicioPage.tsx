74be0ebd63b06900c7c7ae7980ab3c95e3adf8b4
mmanna3 <mmmannna3@gmail.com>
2023-04-25 23:24:24 -0300

    Tunea el banner

--- src/components/pages/InicioPage.tsx
+++ src/components/pages/InicioPage.tsx
@@ -1,5 +1,6 @@
 import { ImageBtn } from '../common/MyButton';
 import BANNER_CELU from '../../assets/images/mobile/banner-celu.png';
+import BANNER_DESKTOP from '../../assets/images/desktop/banner-desktop.jpg';
 import IMG_BTN_TORNEOS from '../../assets/images/mobile/buttons/torneos.png';
 import IMG_BTN_COPAS from '../../assets/images/mobile/buttons/copas.png';
 import IMG_BTN_FICHAJE from '../../assets/images/mobile/buttons/fichaje.png';
@@ -9,7 +10,8 @@ import IMG_BTN_CONTACTO from '../../assets/images/mobile/buttons/contacto.png';
 export const InicioPage = () => {
   return (
     <>
-      <img className='m-auto w-[250px]' src={BANNER_CELU} alt='banner-image' />
+      <img className='m-auto w-[250px] md:hidden' src={BANNER_CELU} alt='banner-image' />
+      <img className='m-auto hidden w-4/5 md:block' src={BANNER_DESKTOP} alt='banner-image' />
       <div className='my-10 flex flex-col items-center gap-6'>
         <ImageBtn img={IMG_BTN_TORNEOS} alt='torneos' url='/torneos' />
         <ImageBtn img={IMG_BTN_COPAS} alt='copas' url='/copas' />
