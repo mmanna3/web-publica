7ee00641593917dcfc1a59694efaf653a41544dc
Elroli09 <yoldi.gustavo@gmail.com>
2023-04-27 18:09:49 -0300

    Agrega contenido a CopasPage

--- src/components/pages/CopasPage.tsx
+++ src/components/pages/CopasPage.tsx
@@ -1,7 +1,15 @@
+import IMG_COPAS_LARGE from '../../assets/images/mobile/buttons/copas-large.png';
+import IMG_COPA_EDEFI from '../../assets/images/mobile/buttons/copa-edefi.png';
+import IMG_COPA_LIGA from '../../assets/images/mobile/buttons/copa-liga.png';
+
 export const CopasPage = () => {
   return (
     <main>
-      <h1 className='mt-60 text-center text-3xl font-bold underline'>CopasPage</h1>
+      <img src={IMG_COPAS_LARGE} alt='Copas' />
+      <div className='flex flex-col gap-16 my-32'>
+        <img className='max-w-lg mx-auto' src={IMG_COPA_EDEFI} alt='Copa-edefi' />
+        <img className='max-w-lg mx-auto' src={IMG_COPA_LIGA} alt='Copa-liga' />
+      </div>
     </main>
   );
 };
