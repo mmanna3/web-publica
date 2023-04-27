f64f15167e3326ce814aea29e7f4679e61479316
Elroli09 <yoldi.gustavo@gmail.com>
2023-04-27 17:27:45 -0300

    Arregla estilos de ContactoPage

--- src/components/pages/ContactoPage.tsx
+++ src/components/pages/ContactoPage.tsx
@@ -1,37 +1,33 @@
-import IMG_CONTACTO_LARGE from '../../assets/images/mobile/buttons/contacto-large.png'
-import SEGUINOS_CELU from '../../assets/images/mobile/seguinos-celu.png'
-import DATOS_CONTACTO_CELU from '../../assets/images/mobile/datos-contacto-celu.png'
+import IMG_CONTACTO_LARGE from '../../assets/images/mobile/buttons/contacto-large.png';
+import SEGUINOS_CELU from '../../assets/images/mobile/seguinos-celu.png';
 
 export const ContactoPage = () => {
   return (
     <main>
-      <img src={IMG_CONTACTO_LARGE} alt="Contacto" />
-      <img src={DATOS_CONTACTO_CELU} alt="Datos" />
-      {/* <div>
-        <h2>Horarios de atención</h2>
-        <p>Lunes a jueves de 17:30 hs a 20:30 hs</p>
-        <p>Viernes de 17:00 hs a 19:30 hs</p>
+      <img className='m-auto' src={IMG_CONTACTO_LARGE} alt='Contacto' />
+      <div className='mx-auto my-12 text-center'>
+        <div className='mb-16 md:text-xl lg:text-2xl'>
+          <h2 className='my-4 text-title-darkGreen md:text-xl lg:text-2xl '>
+            Horarios de atención
+          </h2>
+          <p className='mb-3'>Lunes a jueves de 17:30 hs a 20:30 hs</p>
+          <p className='mb-3'>Viernes de 17:00 hs a 19:30 hs</p>
+        </div>
+        <div className='mb-16 md:text-xl lg:text-2xl'>
+          <h2 className='my-4 text-title-darkGreen md:text-xl lg:text-2xl '>Dirección</h2>
+          <p className='mb-3'>Juan B. Justo 550, Haedo. Bs. As</p>
+          <p className='mb-3'>CP: 1706</p>
+        </div>
+        <div className='mb-16 md:text-xl lg:text-2xl'>
+          <h2 className='my-4 text-title-darkGreen md:text-xl lg:text-2xl '>Teléfono</h2>
+          <p className='mb-3'>21958389</p>
+        </div>
+        <div className='mb-16 md:text-xl lg:text-2xl'>
+          <h2 className='my-4 text-title-darkGreen md:text-xl lg:text-2xl '>E-mail</h2>
+          <p className='mb-3'>edefiargentina@hotmail.com</p>
+        </div>
       </div>
-      <div>
-        <h2>Dirección</h2>
-        <p>Juan B. Justo 550, Haedo. Bs. As</p>
-        <p>CP: 1706</p>
-      </div>
-      <div>
-        <h2>Teléfono</h2>
-        <p>21958389</p>
-      </div>
-      <div>
-        <h2>E-mail</h2>
-        <p>edefiargentina@hotmail.com</p>
-      </div> */}
-      <img src={SEGUINOS_CELU} alt="Seguinos" />
+      <img className='m-auto w-40 md:w-52 lg:w-64' src={SEGUINOS_CELU} alt='Seguinos' />
     </main>
   );
 };
-
-
-
-
-
-
