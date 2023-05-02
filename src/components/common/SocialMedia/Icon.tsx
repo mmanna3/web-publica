a8ecb0a99d70f7aa767e55fb62eaa201f1f25fbb
Elroli09 <yoldi.gustavo@gmail.com>
2023-05-02 15:48:23 -0300

    Cambia tamaño de Social Icons.

--- src/components/common/SocialMedia/Icon.tsx
+++ src/components/common/SocialMedia/Icon.tsx
@@ -3,5 +3,5 @@ type IconProps = {
 };
 
 export const Icon = ({ src }: IconProps) => {
-  return <img className='max-w-[35px]' src={src} alt='instagram' />;
+  return <img className='w-10' src={src} alt='instagram' />;
 };
