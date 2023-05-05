fa5dcd6a415d333fa5cf0d799769891ded1b6ec6
Elroli09 <yoldi.gustavo@gmail.com>
2023-05-05 14:13:56 -0300

    Completa el test de locig.ts

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -4,8 +4,8 @@ import { Outlet } from 'react-router-dom';
 
 export const Layout = () => {
   return (
-    // <div className='bg-fondo-celu bg-cover bg-no-repeat bg-fixed md:bg-fondo-pc'>
-    <div className=' bg-black'>
+    <div className='bg-fondo-celu bg-cover bg-no-repeat bg-fixed md:bg-fondo-pc'>
+    {/* <div className=' bg-black'> */}
       <Header />
       <main className='mx-auto ml-[5%] select-none '>
         <Outlet />
--- src/components/common/logic.test.ts
+++ src/components/common/logic.test.ts
@@ -3,7 +3,7 @@ import { filterTorneosByType } from './logic';
 import { Torneo } from '../../interfaces/Torneo';
 
 describe('filterTorneosByType', () => {
-  it('Filter "FUTSAL" correctly', ({ expect }) => {
+  it('Filter "futsal" correctly', ({ expect }) => {
     // Arrange
     const torneos: Torneo[] = [
       { descripcion: 'MATUTINO 6 CATEGORÍAS', id: '1', formato: 'aperturaClausura' },
@@ -34,5 +34,20 @@ describe('filterTorneosByType', () => {
     expect(resultado.length).toBe(2);
     expect(resultado[0].descripcion).toContain('MATUTINO');
     expect(resultado[1].descripcion).toContain('MATUTINO');
+  });
+
+  it('Filter "futbol11" correctly', ({ expect }) => {
+    // Arrange
+    const torneos: Torneo[] = [
+      { descripcion: 'MATUTINO 6 CATEGORÍAS', id: '1', formato: 'aperturaClausura' },
+      { descripcion: 'FUTSAL MAYORES', id: '3', formato: 'aperturaClausura' },
+      { descripcion: 'MATUTINO 5 CATEGORÍAS', id: '2', formato: 'aperturaClausura' },
+    ];
+
+    // Act
+    const resultado = filterTorneosByType(torneos, 'futbol11');
+
+    // Assert
+    expect(resultado.length).toBe(0);
   });
 });
