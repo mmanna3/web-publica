5a4cdbc34b5439de9333816f00380aa7c608aea1
Elroli09 <yoldi.gustavo@gmail.com>
2023-05-07 10:51:45 -0300

    Elimina componente Layout, lo reemplaza por App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import './App.css';
-import { Layout } from './components/common/Layout';
+import { Outlet } from 'react-router-dom';
+import Header from './components/common/Header';
+import { SocialMedia } from './components/common/SocialMedia/SocialMedia';
 
 export function loader() {
   console.log('soy el loader');
 }
 
 function App() {
-  return <Layout />;
+  return (
+    <div className='max-h-max bg-fondo-celu bg-cover bg-no-repeat'>
+      <Header />
+      <main className='mx-auto ml-[5%] select-none '>
+        <Outlet />
+      </main>
+      <SocialMedia />
+    </div>
+  );
 }
 
 export default App;
