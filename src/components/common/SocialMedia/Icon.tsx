8585efd2d79cb4a660716de7e023c6a208ad3080
Elroli09 <yoldi.gustavo@gmail.com>
2023-05-10 13:12:42 -0300

    SocialMediaIcons ahora son links

--- src/components/common/SocialMedia/Icon.tsx
+++ src/components/common/SocialMedia/Icon.tsx
@@ -1,7 +1,12 @@
 type IconProps = {
   src: string;
+  url: string;
 };
 
-export const Icon = ({ src }: IconProps) => {
-  return <img className='w-10' src={src} alt='instagram' />;
+export const Icon = ({ src, url }: IconProps) => {
+  return (
+    <a href={url} target='_blank' rel='noreferrer'>
+      <img className='w-10' src={src} alt='instagram' />
+    </a>
+  );
 };
--- src/components/common/SocialMedia/SocialMedia.tsx
+++ src/components/common/SocialMedia/SocialMedia.tsx
@@ -6,8 +6,8 @@ import WP_ICON from '../../../assets/images/icons/wp-icon.png';
 export const SocialMedia = () => {
   return (
     <div className='ml-[5%] select-none flex justify-center space-x-3 py-8 pb-10'>
-      <Icon src={IG_ICON} />
-      <Icon src={WP_ICON} />
+      <Icon src={IG_ICON} url='https://www.instagram.com/liga_edefi/'/>
+      <Icon src={WP_ICON} url='watsap'/>
     </div>
   );
 };
