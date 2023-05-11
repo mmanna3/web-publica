3a58c425ec83e724be6a53f1dbedcdcca21b8b4b
Elroli09 <yoldi.gustavo@gmail.com>
2023-05-10 21:25:58 -0300

    Cambia el font-size

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,13 +8,17 @@ module.exports = {
     'font-family': 'Coalition',
   },
   theme: {
+    fontSize: {
+      'xs': '12px',
+    },
+
     extend: {
       backgroundImage: {
         'fondo-pc': "url('assets/images/desktop/fondo-pc.jpg')",
         'fondo-celu': "url('assets/images/mobile/fondo-celu.jpg')",
       },
       colors: {
-        'title-darkGreen': '#579e74'
+        'title-darkGreen': '#579e74',
       },
     },
   },
