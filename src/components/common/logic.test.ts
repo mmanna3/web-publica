daf88b78804523b4ad2f6c4fa1a41a57ec89879a
mmanna3 <mmmannna3@gmail.com>
2023-05-10 23:22:02 -0300

    Agrega lógica para copas a función filterTorneosByType (y sus tests)

--- src/components/common/logic.test.ts
+++ src/components/common/logic.test.ts
@@ -3,6 +3,58 @@ import { filterTorneosByType } from './logic';
 import { Torneo } from '../../interfaces/api';
 
 describe('filterTorneosByType', () => {
+  it('Filter "COPA EDEFI" correctly', ({ expect }) => {
+    // Arrange
+    const torneos: Torneo[] = [
+      { descripcion: 'COPA EDEFI Vespertino', id: '1', formato: 'relampago' },
+      { descripcion: 'COPA EDEFI Matutino', id: '3', formato: 'relampago' },
+      { descripcion: 'COPA DE LA LIGA Bla bla bla', id: '2', formato: 'relampago' },
+      { descripcion: 'TORNEO DE VERANO 5 CAT', id: '4', formato: 'relampago' },
+    ];
+
+    // Act
+    const resultado = filterTorneosByType(torneos, 'copaEdefi');
+
+    // Assert
+    expect(resultado.length).toBe(2);
+    expect(resultado[0].descripcion).toBe('COPA EDEFI Vespertino');
+    expect(resultado[1].descripcion).toBe('COPA EDEFI Matutino');
+  });
+
+  it('Filter "TORNEO DE VERANO" correctly', ({ expect }) => {
+    // Arrange
+    const torneos: Torneo[] = [
+      { descripcion: 'COPA EDEFI Vespertino', id: '1', formato: 'relampago' },
+      { descripcion: 'COPA EDEFI Matutino', id: '3', formato: 'relampago' },
+      { descripcion: 'COPA DE LA LIGA Bla bla bla', id: '2', formato: 'relampago' },
+      { descripcion: 'TORNEO DE VERANO 5 CAT', id: '4', formato: 'relampago' },
+    ];
+
+    // Act
+    const resultado = filterTorneosByType(torneos, 'torneoDeVerano');
+
+    // Assert
+    expect(resultado.length).toBe(1);
+    expect(resultado[0].descripcion).toBe('TORNEO DE VERANO 5 CAT');
+  });
+
+  it('Filter "COPA DE LA LIGA" correctly', ({ expect }) => {
+    // Arrange
+    const torneos: Torneo[] = [
+      { descripcion: 'COPA EDEFI Vespertino', id: '1', formato: 'relampago' },
+      { descripcion: 'COPA EDEFI Matutino', id: '3', formato: 'relampago' },
+      { descripcion: 'COPA DE LA LIGA Bla bla bla', id: '2', formato: 'relampago' },
+      { descripcion: 'TORNEO DE VERANO 5 CAT', id: '4', formato: 'relampago' },
+    ];
+
+    // Act
+    const resultado = filterTorneosByType(torneos, 'copaDeLaLiga');
+
+    // Assert
+    expect(resultado.length).toBe(1);
+    expect(resultado[0].descripcion).toBe('COPA DE LA LIGA Bla bla bla');
+  });
+
   it('Filter "futsal" correctly', ({ expect }) => {
     // Arrange
     const torneos: Torneo[] = [
--- src/components/common/logic.ts
+++ src/components/common/logic.ts
@@ -2,12 +2,15 @@ import { Torneo } from '../../interfaces/api';
 
 export const filterTorneosByType = (
   torneos: Torneo[],
-  tipo: 'baby' | 'futsal' | 'futbol11',
+  tipo: 'baby' | 'futsal' | 'futbol11' | 'copaEdefi' | 'torneoDeVerano' | 'copaDeLaLiga',
 ): Torneo[] => {
   const tiposDeTorneo = {
     baby: ['MATUTINO', 'VESPERTINO'],
     futsal: ['FUTSAL'],
     futbol11: ['FUTBOL 11'],
+    copaEdefi: ['COPA EDEFI'],
+    torneoDeVerano: ['TORNEO DE VERANO'],
+    copaDeLaLiga: ['COPA DE LA LIGA'],
   };
 
   const torneosByType: Torneo[] = [];
