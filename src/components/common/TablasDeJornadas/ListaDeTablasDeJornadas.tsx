84ef53da93e28c99ca1df3c4797a2ed791621da8
Elroli09 <yoldi.gustavo@gmail.com>
2023-05-13 22:00:33 -0300

    Muestra TablasDeJornadas

--- src/components/common/TablasDeJornadas/ListaDeTablasDeJornadas.tsx
+++ src/components/common/TablasDeJornadas/ListaDeTablasDeJornadas.tsx
@@ -1,29 +1,68 @@
-import { useFetchJornadas } from '../../../hooks/useFetchJornadas';
-import { Spinner } from '../Spinner'
-import { TablaDeJornadas } from './TablaDeJornadas';
+import { useParams } from 'react-router-dom';
+import { useFetch } from '../../../hooks/useFetch';
+import { Spinner } from '../Spinner';
+import { RowContent, Table, TableRow } from '../Table';
+import { JornadasDelTorneo } from '../../../interfaces/api';
 
 export const ListaDeTablasDeJornadas = () => {
-  const { jornadasPorFecha, categorias, isFetching } = useFetchJornadas();
+  const { zonaId } = useParams();
+  const { data, isFetching } = useFetch(`jornadas?zonaId=${zonaId}`);
+
+  const jornadas: JornadasDelTorneo = data;
 
   if (isFetching) {
     return <Spinner />;
   }
   return (
-    <div className='flex flex-col justify-center bg-gray-400 p-6'>
-      <div className='mb-10 flex justify-between'>
-        <button className='bg-red-600'>Torneo tanto</button>
-        <button className='bg-green-500'>Zona tanto</button>
-      </div>
-      <div className=''>
-        {jornadasPorFecha.map(({ FechaId, FechaNumero, Renglones }) => (
-          <TablaDeJornadas
-            key={FechaId}
-            renglones={Renglones}
-            categorias={categorias}
-            fechaNumero={FechaNumero}
-          />
-        ))}
-      </div>
+    <div className='mx-auto grid gap-2 sm:grid-cols-2'>
+      {jornadas.JornadasPorFecha.map(({ FechaId, FechaNumero, Renglones }) => (
+        <Table key={FechaId} titulo={`${FechaNumero}`}>
+          <thead>
+            <TableRow type={'tableHead'}>
+              <RowContent content={''} />
+              <RowContent content={'Esc'} />
+              <RowContent content={'Equipo'} />
+              <RowContent content={'1ra'} />
+              <RowContent content={'Rva.'} />
+              <RowContent content={'3ra'} />
+              <RowContent content={'4ta'} />
+              <RowContent content={'5ta'} />
+              <RowContent content={'T.P.'} />
+              <RowContent content={'P.J.'} />
+              <RowContent content={'V'} />
+            </TableRow>
+          </thead>
+          <tbody>
+            {Renglones.map(
+              (
+                {
+                  JornadaId,
+                  JornadaNumero,
+                  Escudo,
+                  Equipo,
+                  ResultadosPorCategorias,
+                  PuntosTotales,
+                  PartidosJugados,
+                  PartidoVerificado,
+                },
+                i,
+              ) => (
+                <TableRow key={JornadaId * i}>
+                  <RowContent content={JornadaNumero} />
+                  <RowContent type={'Img'} content={Escudo} />
+                  <RowContent content={Equipo} />
+                  {ResultadosPorCategorias.map(({ Goles, Orden }, i) => (
+                    <RowContent key={Orden * i} content={Goles} />
+                  ))}
+                  <RowContent content={PuntosTotales} />
+                  <RowContent content={PartidosJugados} />
+                  <RowContent content={PartidoVerificado} />
+                </TableRow>
+              ),
+            )}
+          </tbody>
+        </Table>
+      ))}
     </div>
   );
 };
--- src/components/common/TablasDePosiciones/ListaDeTablasDePosiciones.tsx
+++ src/components/common/TablasDePosiciones/ListaDeTablasDePosiciones.tsx
@@ -2,14 +2,14 @@ import { useParams } from 'react-router-dom';
 import { useFetch } from '../../../hooks/useFetch';
 import { Spinner } from '../Spinner';
 import { RowContent, Table, TableRow } from '../Table';
-import { PosicionesDelTorneo, Tabla } from '../../../interfaces/api';
+import { PosicionesDelTorneo, Renglon, Tabla } from '../../../interfaces/api';
 
 /* //ESTE COMPONENTE SE VA A ENCARGAR DE FILTRAR LA DATA Y PASARSELA A LA TABLA GENERICA. */
 
 export const ListaDeTablasDePosiciones = () => {
   const { zonaId } = useParams();
-  const { data, isFetching } = useFetch<PosicionesDelTorneo>(`/posiciones?zonaId=${zonaId}`);
-  const { TablaGeneral, TablasPorCategoria } = data;
+  const { data, isFetching } = useFetch(`/posiciones?zonaId=${zonaId}`);
+  const { TablaGeneral, TablasPorCategoria }:PosicionesDelTorneo = data;
 
   if (isFetching) {
     return <Spinner />;
@@ -50,10 +50,38 @@ export const ListaDeTablasDePosiciones = () => {
         </Table>
       ))}
 
-      {/* <Table key={TablaGeneral.CategoriaId} titulo='General'>
-        <TableHead columnNames={['Pos', 'Esc', 'Equipo', 'J', 'G', 'E', 'P', 'Np', 'Pts']} />
-        <tbody><TableRow row={TablaGeneral.Renglones[0]}/></tbody>
-      </Table> */}
+      <Table key={TablaGeneral.CategoriaId} titulo={'General'}>
+        <thead>
+          <TableRow type={'tableHead'}>
+            <RowContent content={'Pos'} />
+            <RowContent content={'Esc'} />
+            <RowContent content={'Equipo'} />
+            <RowContent content={'J'} />
+            <RowContent content={'G'} />
+            <RowContent content={'E'} />
+            <RowContent content={'P'} />
+            <RowContent content={'Np'} />
+            <RowContent content={'Pts'} />
+          </TableRow>
+        </thead>
+        <tbody>
+          {TablaGeneral.Renglones.map(
+            ({ EquipoId, Posicion, Escudo, Equipo, Pj, Pg, Pe, Pp, Np, Pts }: Renglon) => (
+              <TableRow key={EquipoId}>
+                <RowContent content={Posicion} />
+                <RowContent type={'Img'} content={Escudo} />
+                <RowContent content={Equipo} />
+                <RowContent content={Pj} />
+                <RowContent content={Pg} />
+                <RowContent content={Pe} />
+                <RowContent content={Pp} />
+                <RowContent content={Np} />
+                <RowContent content={Pts} />
+              </TableRow>
+            ),
+          )}
+        </tbody>
+      </Table>
     </div>
   );
 };
