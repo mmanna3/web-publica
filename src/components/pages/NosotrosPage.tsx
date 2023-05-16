095bdd083eb9fe0b0a4a0421396eb9c56baae85c
Elroli09 <yoldi.gustavo@gmail.com>
2023-05-15 23:22:13 -0300

    NosotrosPage responsive

--- src/components/pages/NosotrosPage.tsx
+++ src/components/pages/NosotrosPage.tsx
@@ -1,26 +1,36 @@
+import { ReactNode } from 'react';
 import IMG_NOSOTROS_TITLE from '../../assets/images/mobile/titles/nosotros-title.png';
+import { Title } from '../common/Title';
+
+interface Props {
+  children: ReactNode;
+}
+const Text = ({ children }: Props) => {
+  return <p className='mb-6 font-arial'>{children}</p>;
+};
 
 export const NosotrosPage = () => {
   return (
     <>
-      <img className='mx-auto' src={IMG_NOSOTROS_TITLE} alt='Nosotros' />
+      <Title img={IMG_NOSOTROS_TITLE} alt='Nosotros' />
       <div className='mx-6 md:mx-20'>
-        <h2 className='my-6 text-title-darkGreen md:text-xl lg:text-3xl '>Quiénes somos</h2>
-        <div className='bg-white text-justify opacity-90 p-4 sm:p-8 lg:p-14 text-sm md:text-lg lg:text-2xl '>
-          <div className= 'mb-10'>
-            <p className='mb-6 font-sans'>
+        <h2 className='mb-6 mt-12 text-title-darkGreen md:text-xl lg:text-3xl '>Quiénes somos</h2>
+        <div className=' rounded-xl bg-gradient-to-tl from-title-darkGreen to-slate-100 p-6 text-justify text-sm opacity-70 sm:p-8 md:text-lg lg:p-14 lg:text-2xl '>
+          <div className='mb-10'>
+            <Text>
               Nuestra Liga es dirigida por Profesores de Educación Física, donde además de ser
               docentes en el ámbito escolar tenemos una amplia trayectoria y recorrido por el ámbito
               del fútbol, desde profes de baby hasta directores técnicos y preparadores físicos en
               fútbol 11 de diferentes clubes, trabajando hace más de 15 años con chicos de todas las
               edades, desde niños de 3 años hasta jugadores profesionales.
-            </p>
-            <p className='mb-6 font-sans'>
+            </Text>
+            <p className='mb-6 font-arial'></p>
+            <p className='mb-6 font-arial'>
               Nuestra experiencia y formación nos dieron el pie para comenzar esta nueva etapa como
               organizadores de diferentes torneos, nucleando hoy en día gran cantidad de
               Instituciones y promoviendo el deporte y la actividad física a miles de chicos.
             </p>
-            <p className='mb-6 font-sans'>
+            <p className='mb-6 font-arial'>
               Iniciamos éste proyecto en el año 2014, realizando encuentros deportivos entre ocho
               escuelas de fútbol con profes conocidos, jugando los días sábados por la mañana de
               manera amistosa, inculcando valores a través de este hermoso deporte que es el fútbol.
@@ -29,7 +39,7 @@ export const NosotrosPage = () => {
               para cada jugador, publicando tabla de posiciones y resultados, y entregando un trofeo
               para cada chico.
             </p>
-            <p className='mb-6 font-sans'>
+            <p className='mb-6 font-arial'>
               Años siguientes, mejoramos nuestra estructura y organización, incorporando nuevos
               torneos y zonas de baby fútbol, tanto por la mañana como por la tarde; formamos
               nuestro propio staff de árbitros, brindándoles cada año un nuevo curso para aquellos
@@ -39,7 +49,7 @@ export const NosotrosPage = () => {
               finalizaban la etapa del baby fútbol y tenían que irse del club; y brindamos mejor
               calidad de atención a cada uno de los clubes.
             </p>
-            <p className='mb-6 font-sans'>
+            <p className='mb-6 font-arial'>
               Actualmente la liga cuenta con más de 100 equipos donde participan anualmente miles de
               chicos y chicas, de todas las edades, en diferentes torneos, proyectando este lindo
               deporte desde la edad de la niñez, comenzando por el baby fútbol, pasando por la
@@ -47,22 +57,18 @@ export const NosotrosPage = () => {
               finalizando la carrera como jugador de Fútsal.
             </p>
           </div>
-          <div className= 'mb-10'>
-            <h2 className='my-6  text-title-darkGreen md:text-xl lg:text-3xl '>
-              Misión
-            </h2>
-            <p className='mb-6 font-sans'>
+          <div className='mb-10'>
+            <h2 className='my-6  text-title-darkGreen md:text-xl lg:text-3xl '>Misión</h2>
+            <p className='mb-6 font-arial'>
               Fomentar, promover, desarollar e impulsar la actividad deportiva a través de la
               organización de torneos, en donde los niños y jóvenes deportistas tengan una actividad
               organizada de calidad y con base en los principios del juego limpio, que fomenten la
               salud y la cultura del deporte de las presentes y nuevas generaciones.{' '}
             </p>
           </div>
-          <div className= 'mb-10'>
-            <h2 className='my-6 text-title-darkGreen md:text-xl lg:text-3xl '>
-              Visión
-            </h2>
-            <p className='mb-6 font-sans'>
+          <div className='mb-10'>
+            <h2 className='my-6 text-title-darkGreen md:text-xl lg:text-3xl '>Visión</h2>
+            <p className='mb-6 font-arial'>
               Ser reconocidos como una Liga líder en la organización de eventos deportivos que
               satisfaga las necesidades de las Instituciones y brinde un espacio de oportunidades
               para el crecimiento integral de nuestros deportistas, padres de familias,
@@ -70,11 +76,9 @@ export const NosotrosPage = () => {
               filosofía y prestigio de LIGA EDEFI.
             </p>
           </div>
-          <div className= 'mb-10'>
-            <h2 className='my-6 text-title-darkGreen md:text-xl lg:text-3xl '>
-              Valores
-            </h2>
-            <p className='mb-6 font-sans'>
+          <div className='mb-10'>
+            <h2 className='my-6 text-title-darkGreen md:text-xl lg:text-3xl '>Valores</h2>
+            <p className='mb-6 font-arial'>
               Empatía, Resolución, Competitividad, Responsabilidad, Honestidad, Amistad,
               Participación, Respeto, Humildad, Compromiso y Decisión.
             </p>
