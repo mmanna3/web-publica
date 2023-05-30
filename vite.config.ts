da6cddee1f034a8ffa77c589a708633b18758db0
Elroli09 <yoldi.gustavo@gmail.com>
2023-05-30 15:26:05 -0300

    Setea las funciones de vitest como globales y sus tipos.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 /* eslint-disable spaced-comment */
 /// <reference types="vitest" />
+/// <reference types="Vite/client" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
-    // ...
+    globals: true,
   },
   plugins: [react()],
 });
