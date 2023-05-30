38baab7b6e9d03363e8c5516dcab4a9e0edbbf3f
Elroli09 <yoldi.gustavo@gmail.com>
2023-05-30 17:19:53 -0300

    Hasta acá andan los test

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
+    environment: 'jsdom',
     globals: true,
   },
   plugins: [react()],
