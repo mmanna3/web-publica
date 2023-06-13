1959ecef45b916417706d949d4b03d3f7d3f305c
Elroli09 <yoldi.gustavo@gmail.com>
2023-06-13 15:24:47 -0300

    Agrega Email-icon con funcionalidad copiar e-mail.

--- src/components/common/SocialMediaIcons.tsx
+++ src/components/common/SocialMediaIcons.tsx
@@ -1,24 +1,44 @@
 import IG_ICON from '../../assets/images/icons/ig-icon.avif';
-import WP_ICON from '../../assets/images/icons/wp-icon.avif';
+import EMAIL_ICON from '../../assets/images/icons/email-icon.avif';
+import { ReactNode } from 'react';
 
 export const SocialMediaIcons = () => {
   return (
-    <div className='mx-auto flex select-none justify-center space-x-3 p-4'>
-      <Icon src={IG_ICON} url='https://www.instagram.com/liga_edefi/' />
-      <Icon src={WP_ICON} url='https://wa.me/1158900516' />
+    <div className='mx-auto flex select-none items-center justify-center space-x-3 p-4'>
+      <InstagramIcon />
+      <CopyToClipboard>
+        <EmailIcon />
+      </CopyToClipboard>
     </div>
   );
 };
 
-interface IconProps {
-  src: string;
-  url: string;
-}
-
-const Icon = ({ src, url }: IconProps) => {
+export const InstagramIcon = () => {
   return (
-    <a href={url} target='_blank' rel='noreferrer'>
-      <img className='w-8' src={src} alt='social-icon' />
+    <a href='https://www.instagram.com/liga_edefi/' target='_blank' rel='noreferrer'>
+      <img className='w-8' src={IG_ICON} alt='instagram-icon' />
     </a>
   );
+};
+
+const EmailIcon = () => {
+  return <img className='w-10' src={EMAIL_ICON} alt='email-icon' />;
+};
+
+interface CopyToClipboardProps {
+  children: ReactNode;
+}
+
+function copy() {
+  const email = 'edefiargentina@hotmail.com';
+  navigator.clipboard.writeText(email);
+}
+
+function handleClick() {
+  copy();
+  alert('E-mail copiado al portapapeles!')
+}
+
+const CopyToClipboard = ({ children }: CopyToClipboardProps) => {
+  return <button onClick={handleClick}>{children}</button>;
 };
