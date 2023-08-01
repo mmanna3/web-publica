65c5a2b6f02e3458dba8323bcf5d4c604204115d
mmanna3 <mmmannna3@gmail.com>
2023-08-01 20:58:29 -0300

    Si es anual, muestra solo la opción 'posiciones'

--- src/components/pages/Torneos/OpcionesDeLaZona.tsx
+++ src/components/pages/Torneos/OpcionesDeLaZona.tsx
@@ -4,17 +4,21 @@ import { GenericButton } from '../../common/GenericButton';
 export const OpcionesDeLaZona = () => {
   const { torneoId, zonaId } = useParams();
   const [URLSearchParams] = useSearchParams();
+  const esAnual = URLSearchParams.get('esAnual');
 
   return (
     <>
       <div className='flex flex-col items-center gap-5'>
         <GenericButton
-          path={`/torneo/${torneoId}/zona/${zonaId}/posiciones?esAnual=${URLSearchParams.get('esAnual')}`}
+          path={`/torneo/${torneoId}/zona/${zonaId}/posiciones?esAnual=${esAnual}`}
           content='Posiciones'
         />
-        <GenericButton path={`/torneo/${torneoId}/zona/${zonaId}/fixture`} content='Fixture' />
-        <GenericButton path={`/torneo/${torneoId}/zona/${zonaId}/jornadas`} content='Jornadas' />
-        <GenericButton path={`/torneo/${torneoId}/zona/${zonaId}/clubes`} content='Clubes' />
+        {esAnual !== 'true' &&
+        <>
+          <GenericButton path={`/torneo/${torneoId}/zona/${zonaId}/fixture`} content='Fixture' />
+          <GenericButton path={`/torneo/${torneoId}/zona/${zonaId}/jornadas`} content='Jornadas' />
+          <GenericButton path={`/torneo/${torneoId}/zona/${zonaId}/clubes`} content='Clubes' /> 
+        </>}
       </div>
     </>
   );
