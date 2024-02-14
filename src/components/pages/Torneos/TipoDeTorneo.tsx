50929d499edd8c9bb3ecc45f7a743eb58c5a6733
mmanna3 <mmmannna3@gmail.com>
2024-02-14 01:27:19 -0300

    Se ven los torneos del año pasado

--- src/components/pages/Torneos/TipoDeTorneo.tsx
+++ src/components/pages/Torneos/TipoDeTorneo.tsx
@@ -3,6 +3,7 @@ import { Torneo } from '../../../interfaces/api';
 import { filterTorneosByType } from '../../common/logic';
 import { Spinner } from '../../common/Spinner';
 import { GenericButton } from '../../common/GenericButton';
+import { useState } from 'react';
 
 interface Props {
   tipo: 'baby' | 'futsal' | 'futbol11';
@@ -10,9 +11,12 @@ interface Props {
 
 export const TipoDeTorneo = ({ tipo }: Props) => {
   const anioActual = new Date().getFullYear();
+  const anioPasado = anioActual - 1;
   const { data, isFetching } = useFetch<Torneo>(`TorneosAperturaClausura?anio=${anioActual}`);
+  const { data: torneosAnioPasado, isFetching: isFetchingAnioPasado } = useFetch<Torneo>(`TorneosAperturaClausura?anio=${anioPasado}`);
+  const [seVenLosDelAnioPasado, verAnioPasado] = useState(false);
 
-  if (isFetching) {
+  if (isFetching || isFetchingAnioPasado) {
     return <Spinner />;
   }
 
@@ -22,6 +26,14 @@ export const TipoDeTorneo = ({ tipo }: Props) => {
         {filterTorneosByType(data, tipo).map(({ id, descripcion }) => (
           <GenericButton key={id} path={`/torneo/${id}/zonas`} content={descripcion} />
         ))}
+      </div>
+      <div className='flex flex-col items-center gap-10 mt-20 mb-10'>
+        <button className='text-sm border p-2 border-slate-700 rounded text-slate-600' onClick={() => verAnioPasado(v => !v)}>Año {anioPasado}</button>
+      </div>
+      <div className='flex flex-col items-center gap-10'>                
+        {seVenLosDelAnioPasado ? filterTorneosByType(torneosAnioPasado, tipo).map(({ id, descripcion }) => (
+          <GenericButton key={id} path={`/torneo/${id}/zonas`} content={descripcion} />
+        )): null}
       </div>
     </>
   );
