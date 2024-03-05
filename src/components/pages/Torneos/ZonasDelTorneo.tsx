10f18ef5e7578ef81f0ab0bfd862e3e68168ca9a
mmanna3 <mmmannna3@gmail.com>
2024-03-05 03:49:19 -0300

    Se muestra la llave eliminatoria si el torneo tiene una

--- src/components/pages/Torneos/ZonasDelTorneo.tsx
+++ src/components/pages/Torneos/ZonasDelTorneo.tsx
@@ -4,18 +4,35 @@ import { Zona } from '../../../interfaces/api';
 import { GenericButton } from '../../common/GenericButton';
 import { Spinner } from '../../common/Spinner';
 import { flatZonas } from '../../common/logic';
+import { useEffect } from 'react';
 
 export const ZonasDelTorneo = () => {
   const { torneoId } = useParams();
   const { data, isFetching } = useFetch<Zona>(`zonas?torneoId=${torneoId}`);
+  const { data: nombreLlaveEliminatoria, isFetching: isFetchingTieneLlave } = useFetch(`tieneLlaveEliminatoria?torneoId=${torneoId}`);
 
-  if (isFetching) {
+  useEffect(() => {
+    if ((nombreLlaveEliminatoria as unknown as string) !== '')
+      console.log('nombre llave:', nombreLlaveEliminatoria)
+    else
+      console.log('la llave no tiene nombre', nombreLlaveEliminatoria)
+    
+  }, [nombreLlaveEliminatoria])
+  
+
+  if (isFetching || isFetchingTieneLlave) {
     return <Spinner />;
   }
 
   return (
     <>
       <div className='flex flex-col items-center gap-10'>
+        {(nombreLlaveEliminatoria as unknown as string) !== '' && <GenericButton
+            key={'llave-eliminatoria'}
+            path={`/torneo/${torneoId}/llaveeliminatoria`}
+            content={nombreLlaveEliminatoria.toString()}
+          />}
+
         {flatZonas(data).map((zona) => (
           <GenericButton
             key={zona?.id + zona.esAnual.toString()}
